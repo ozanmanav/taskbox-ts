3905745800693e0d257a7cf9a6f95ed8f33e8e65
Ozan Manav <ozan.manavv@gmail.com>
2020-01-01 14:28:08 +0300

    chromatic testrevert

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -30,7 +30,7 @@ const Task: React.FC<TaskProps> = ({
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
-      <div className="title" style={{ background: "green" }}>
+      <div className="title">
         <input
           type="text"
           value={title}
